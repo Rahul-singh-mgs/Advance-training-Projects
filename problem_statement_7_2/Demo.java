f35a6b8a6bf18a0a44406bfa135ad094710d5251
Rahul Singh <rahulsingh.mgs1998@gmail.com>
2022-04-29 16:16:03 +0530

    first commit

---
+++ problem_statement_7_2/Demo.java
@@ -0,0 +1,9 @@
+package problem_statement_7_2;
+
+class NameNotValidException extends Exception
+{
+     public String validname()
+     {
+          return ("Name is not Valid..Please ReEnter the Name");
+     }
+}
